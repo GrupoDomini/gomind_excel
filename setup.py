15fd4fd1ab810b5256b24a0b776c554094a72357
JeffersonCarvalhoGM <152503266+JeffersonCarvalhoGM@users.noreply.github.com>
2024-05-23 11:24:58 -0300

    Update setup.py V1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="gomind_excel",
     python_requires=">=3.6",
-    version="0.0.0",
+    version="1.0.0",
     description="GoMind excel functions",
     url="https://github.com/GrupoDomini/gomind_excel.git",
     author="JeffersonCarvalhoGD",
